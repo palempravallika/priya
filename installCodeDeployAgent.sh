24992a027f8826fda27158bb28390098ccb8e0d6
palempravallika <palempravallika09@gmail.com>
2025-04-12 11:44:58 +0530

    Create installCodeDeployAgent.sh

---
+++ installCodeDeployAgent.sh
@@ -0,0 +1,8 @@
+sudo yum update -y
+sudo yum install ruby -y
+sudo yum install wget -y
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
